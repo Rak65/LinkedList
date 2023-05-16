999db0bc494127792d4641573bf129b8ab0dfff3
Rak65 <rakeshp6505@gmail.com>
2023-05-16 19:31:28 +0530

    Create a simple Linked List

---
+++ LinkedListDS/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDS
+{
+    public class LinkedList
+    {
+        public static void Add()
+        {
+            LinkedList<int> linked = new LinkedList<int>();
+            linked.AddFirst(56);
+            linked.AddLast(30);
+            linked.AddLast(70);
+
+            foreach (var input in linked)
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
---
+++ LinkedListDS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList Data Structure");
+            LinkedList.Add();
+            Console .ReadKey();
+        }
+    }
+}
